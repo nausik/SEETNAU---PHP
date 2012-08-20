1d7543c93d69d610fea53ad897fac95d08907686
Artem Barbynyagra <xaputo@gmail.com>
2012-08-21 01:07:56 +0300

    Oh god I made a huge mistake

--- js/friends.js
+++ js/friends.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.send_message_b').click(function() {
-		$("#to_name").text($(this).attr('data-name'););
+		$("#to_name").text($(this).attr('data-name'));
 });
 
 	$(".list_title").click(function() {
